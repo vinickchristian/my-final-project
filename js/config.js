a4e78dbdca22f14fd8c9ceb9222f5153a93912f1
vinickchristian <vinickmiamo19@gmail.com>
2026-02-14 11:00:21 +0100

    API update

--- js/config.js
+++ js/config.js
@@ -1,3 +1,3 @@
-export const TMDB_API_KEY = "a1b2c3d4e5f6g7h8i9j0";
+export const TMDB_API_KEY = "19dc28f7c0f8568f68ce61b9bb97d5a2";
 export const TMDB_BASE_URL = "https://api.themoviedb.org/3";
 export const TMDB_IMAGE_BASE = "https://image.tmdb.org/t/p/w500";
